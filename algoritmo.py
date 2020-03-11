835d55707e9afc6977f0a402419b439f6113d971
icarotc <53961451+icarotc@users.noreply.github.com>
2020-03-11 05:47:18 -0300

    Função mix

--- algoritmo.py
+++ algoritmo.py
@@ -4,6 +4,7 @@ from functions_database import get_stock_per_drink, get_stock_per_cluster, get_s
 #from cluster import clusters
 import pandas as pd
 import pdb
+from calculateDistances import deliveries
 
 """
 Legenda:
@@ -40,6 +41,7 @@ def cluster_pedido(clusters, quantidade_pedido):
     for cluster, row in clusters.iterrows():
         names = clusters.loc[cluster, "name"]
         this_cluster = quantidade_pedido[quantidade_pedido.index.isin(names) == True]
+        
         total = this_cluster["quantidade"].sum()
         clusters_command.loc[cluster, "quantidade"] = total
     
@@ -61,11 +63,11 @@ def existe_estoque(depositos_prox, clusters_command, quantidade_pedido):
     """
 
     #Criação de DataFrame para monitorar se há ou não estoque suficiente de cada bebida por depósito
-    df_bebidas = pd.DataFrame({'id':depositos_prox.index,'custo_frete':depositos_prox['custo_frete']}, columns=['id','custo_frete']) 
+    df_bebidas = pd.DataFrame({'id':depositos_prox.index,'custo_frete':depositos_prox['price']}, columns=['id','custo_frete']) 
     df_bebidas.set_index('id', inplace=True)
 
     #Criação de DataFrame para monitorar se há ou não estoque suficiente de cada cluster por depósito
-    df_clusters = pd.DataFrame({'id':depositos_prox.index, 'custo_frete':depositos_prox['custo_frete']}, 
+    df_clusters = pd.DataFrame({'id':depositos_prox.index, 'custo_frete':depositos_prox['price']}, 
                                 columns=['id','custo_frete']) 
     df_clusters.set_index('id', inplace=True)
 
@@ -116,7 +118,6 @@ def combine_stocks(ranking_depositos, quantidade_pedidos):
     stock_1.set_index("drink_name", inplace=True)
     stock_2 = get_stock_per_drink(id_2) #DataFrame com estoque de bebidas do segundo maior deposito
     stock_2.set_index("drink_name", inplace=True)
-
     condition = True
 
     #pdb.set_trace()
@@ -124,57 +125,114 @@ def combine_stocks(ranking_depositos, quantidade_pedidos):
         if row['quantidade'] > stock_1.loc[bebida, "quantity"] + stock_2.loc[bebida, "quantity"]:
             condition = False
             break
-    
-    #Verificação para saber se o tempo de entrega combinado é menor do que 1 dia(tempo em minutos)
-    if depositos_prox.loc[id_1, "tempo_entrega"] + depositos_prox.loc[id_2, "tempo_entrega"] > 1440:
-        condition = False
 
     return condition
 
-if __name__ == "__main__":
-    quantidade_pedido = pd.DataFrame({"bebida":['Antarctica Originial', "Budweiser", "Guarana Antarctica", 
-    "Energetico Fusion Normal", "Energetico Fusion Pessego"], "quantidade":[10, 5, 30, 12, 21]})
-    quantidade_pedido.set_index(["bebida"], inplace=True)
 
-    depositos_prox = pd.DataFrame({"id":[1, 2, 3, 4], "custo_frete":[100, 150, 70, 230], 
-                                    "tempo_entrega":[10, 15, 7, 23]}) #Se o custo for referente ao tempo 
+def mix_bebidas(id_deposito, quantidade_pedido):
+    stock_drinks = get_stock_per_drink(id_deposito)
+    stock_drinks.set_index(["drink_name"], inplace=True)
+    condition = True
+    entrega = {}
+  
+    for bebida, row in quantidade_pedido.iterrows():
+        if quantidade_pedido.loc[bebida, "quantidade"] > stock_drinks.loc[bebida,"quantity"]:
+            entrega[bebida] = stock_drinks.loc[bebida,"quantity"]
+            diferenca = quantidade_pedido.loc[bebida, "quantidade"] - stock_drinks.loc[bebida,"quantity"]
+            stock_drinks.loc[bebida,"quantity"] = 0
+            cluster_bebida = stock_drinks.loc[bebida,"cluster"]
+            mix = stock_drinks[stock_drinks["cluster"] == cluster_bebida]
+            pdb.set_trace()
+            mix.sort_values(by=["price"], inplace=True, ascending=False, ignore_index=False)
+            mix.drop(bebida, axis=0, inplace=True)
+            while diferenca != 0:
+                for bebida2, row2 in mix.iterrows():
+                    if diferenca > mix.loc[bebida2,"quantity"]:
+                        if bebida2 in entrega.keys():
+                            entrega[bebida2] += mix.loc[bebida2,"quantity"]
+                        else:
+                            entrega[bebida2] = mix.loc[bebida2,"quantity"]
+                        diferenca -= mix.loc[bebida2,"quantity"]
+                        stock_drinks.loc[bebida,"quantity"] -= mix.loc[bebida2,"quantity"]
+                    else:
+                        if bebida2 in entrega.keys():
+                            entrega[bebida2] += diferenca
+                        else:
+                            entrega[bebida2] = diferenca
+                        diferenca = 0
+                        stock_drinks.loc[bebida,"quantity"] = diferenca
+                        break
+        else:
+            entrega[bebida] = quantidade_pedido.loc[bebida, "quantidade"]
+            stock_drinks.loc[bebida,"quantity"] -= quantidade_pedido.loc[bebida, "quantidade"]
+            
+    pdb.set_trace()
+    return entrega
+        
+    
+def bussola(quantidade_pedido):
+    deliv = deliveries()
+    depositos_prox = deliv.calculateDistances(-23.6, -46.6, 3)
     depositos_prox.set_index(["id"], inplace=True)
-    #depositos_prox.set_index(["id"], inplace=True)
-    
-    
-    #Calculo dos clusters presentes no pedido
+
+    # Calculo dos clusters presentes no pedido
     clusters = get_clusters()
     clusters_command = cluster_pedido(clusters, quantidade_pedido)
+    quant_pedido = pd.DataFrame(columns = ["bebida","quantidade", "cluster"])
+    quant_pedido.set_index("bebida", inplace=True)
+    
+    for cluster, row in clusters.iterrows():
+        names = row["name"]
+        df = quantidade_pedido[quantidade_pedido.index.isin(names)==True]
+        quant_pedido.append(df)
+       
 
-    #Estabelecimento do limite de preço para conseguirmos entregar ou não no dia D
-    #preco_total = quantidade_pedido['preco'].sum()
+    # Estabelecimento do limite de preço para conseguirmos entregar ou não no dia D
+    # preco_total = quantidade_pedido['preco'].sum()
 
-    #DataFrame que rankeia depositos baseado no total de estoque presente
+    # DataFrame que rankeia depositos baseado no total de estoque presente
     ranking_depositos = pd.DataFrame(columns={"id", "n_estoque"})
     for id in depositos_prox.index:
-        ranking_depositos = ranking_depositos.append({"id":id, "n_estoque":get_stock_total(id)}, ignore_index=True) 
-        
+        ranking_depositos = ranking_depositos.append({"id": id, "n_estoque": get_stock_total(id)}, ignore_index=True)
+
     ranking_depositos.sort_values(by=["n_estoque"], inplace=True, ascending=False, ignore_index=True)
 
-    #Acrescentada condição de cada deposito: 'infull', 'partial' ou 'none'
+    # Acrescentada condição de cada deposito: 'infull', 'partial' ou 'none'
     depositos_prox = existe_estoque(depositos_prox, clusters_command, quantidade_pedido)
-    
-    #DataFrame reorganizado para ter uma ordem baseado no tempo de cada deposito para o cliente
-    depositos_prox.sort_values(by=['tempo_entrega'], axis=0, inplace=True)
 
+    # DataFrame reorganizado para ter uma ordem baseado no tempo de cada deposito para o cliente
+    depositos_prox.sort_values(by=['distance'], axis=0, inplace=True)
+
+    #mix_bebidas(7, quantidade_pedido)
     
     if not depositos_prox[depositos_prox['condition'] == "infull"].empty:
         print("Entregaremos seu pedido em algumas horas")
-    
+
     elif not depositos_prox[depositos_prox['condition'] == "partial"].empty:
-        condition = combine_stocks(ranking_depositos, quantidade_pedido)
-        if condition:
-            print("Temos 2 opções para você: Uma misturamos as bebidas e entregamos hj e outra nao misturamos e \
-                entregamos amanhã")
-        else:
-            print("Podemos misturar as opções?")
-    else :
+        mensagem = ''
+        combine= combine_stocks(ranking_depositos, quantidade_pedido)
+        id_menor = ranking_depositos.loc[0, "id"]
+        mix = mix_bebidas(id_menor, quantidade_pedido)
+        if combine:
+            mensagem = 'Conseguimos entregar hoje com um frete um pouco maior'
+        if mix:
+            mensagem = 'Podemos misturar as opções'
+    else:
         print("Entregaremos apenas amanhã, mas temos um desconto especial para você")
+
+if __name__ == "__main__":
+    quantidade_pedido = pd.DataFrame({"bebida":['Antarctica Originial', "Budweiser", "Guarana Antarctica", 
+    "Energetico Fusion Normal", "Energetico Fusion Pessego"], "quantidade":[100, 50, 300, 120, 210]})
+    quantidade_pedido.set_index(["bebida"], inplace=True)
+    
+    bussola(quantidade_pedido)
+
+    #depositos_prox = pd.DataFrame({"id":[1, 2, 3, 4], "custo_frete":[100, 150, 70, 230], 
+    #                                "tempo_entrega":[10, 15, 7, 23]}) #Se o custo for referente ao tempo 
+    #depositos_prox.set_index(["id"], inplace=True)
+    #depositos_prox.set_index(["id"], inplace=True)
+    
+
 
 #Pegar bebidas mais baratas entre as disponíveis
 #Colocar preço de produto mais frete no retorno ao cliente
--- functions_database.py
+++ functions_database.py
@@ -98,4 +98,4 @@ def get_stock_per_cluster(cdd_id):
     return df_per_cluster[['cluster', 'quantity']]
 
 if __name__=='__main__':
-    print(get_stock_per_drink(1))
+    print(get_clusters())
